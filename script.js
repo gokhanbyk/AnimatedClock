87f37b108ddecc63dde46174f9edf9935f6c8a7b
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-02-15 19:21:08 +0300

    interactive color for clock

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 //! Animated Clock
+
+const faceColor = document.getElementById("face-color");
+const borderColor = document.getElementById("border-color");
+const lineColor = document.getElementById("line-color");
+const largeHandColor = document.getElementById("large-hand-color");
+const secondHandColor = document.getElementById("second-hand-color");
+
 function clock() {
   const now = new Date();
   const canvas = document.getElementById("canvas");
@@ -20,7 +27,8 @@ function clock() {
   ctx.save();
   ctx.beginPath();
   ctx.lineWidth = 14;
-  ctx.strokeStyle = "#800000";
+  ctx.strokeStyle = borderColor.value;
+  ctx.fillStyle = faceColor.value;
   ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
   ctx.stroke();
   ctx.fill();
@@ -28,6 +36,7 @@ function clock() {
 
   // Draw hour lines
   ctx.save();
+  ctx.strokeStyle = lineColor.value;
   for (let i = 0; i < 12; i++) {
     ctx.beginPath();
     ctx.rotate(Math.PI / 6);
@@ -39,6 +48,7 @@ function clock() {
 
   // Draw minute lines
   ctx.save();
+  ctx.strokeStyle = lineColor.value;
   ctx.lineWidth = 4;
   for (let i = 0; i < 60; i++) {
     if (i % 5 !== 0) {
@@ -61,7 +71,7 @@ function clock() {
   ctx.rotate(
     (Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec
   );
-  ctx.strokeStyle = "#800000";
+  ctx.strokeStyle = largeHandColor.value;
   ctx.lineWidth = 12;
   ctx.beginPath();
   ctx.moveTo(-20, 0);
@@ -73,7 +83,7 @@ function clock() {
   // Draw minute hand
   ctx.save();
   ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
-  ctx.strokeStyle = "#800000";
+  ctx.strokeStyle = largeHandColor.value;
   ctx.lineWidth = 10;
   ctx.beginPath();
   ctx.moveTo(-28, 0);
@@ -85,8 +95,8 @@ function clock() {
   // Draw sec hand
   ctx.save();
   ctx.rotate((sec * Math.PI) / 30);
-  ctx.strokeStyle = "#FF7F50";
-  ctx.fillStyle = "#FF7F50";
+  ctx.strokeStyle = secondHandColor.value;
+  ctx.fillStyle = secondHandColor.value;
   ctx.lineWidth = 6;
   ctx.beginPath();
   ctx.moveTo(-30, 0);
